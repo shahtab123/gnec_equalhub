61f787206ffbd39549835365befa8bbe190be6ce
Shahtab Mohtasin <32034315+shahtab123@users.noreply.github.com>
2025-03-28 01:55:06 +0600

    page icon

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -2,5 +2,5 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "EqualHub - A Unified Platform for Equality and Inclusion",
-  description: "Join us in promoting gender equality and reducing inequalities through interactive learning and engagement.",
+  description: "Join us in promoting gender equality and reducing inequalities through interactive learning and engagement."
 }; 
